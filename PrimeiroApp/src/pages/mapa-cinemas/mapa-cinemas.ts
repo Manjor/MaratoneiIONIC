4c93ed10bc0e973974527d80d3534671c87edf1c
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2018-07-09 14:02:45 -0300

    Gerado o Maps
    Gerado pagina de Mapa

--- PrimeiroApp/src/pages/mapa-cinemas/mapa-cinemas.ts
+++ PrimeiroApp/src/pages/mapa-cinemas/mapa-cinemas.ts
@@ -1,97 +1,84 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
-import {Geolocation} from '@ionic-native/geolocation';
-import { DomSanitizer } from '@angular/platform-browser';
-import {GoogleMaps, GoogleMap, GoogleMapOptions, Marker, GoogleMapsEvent} from '@ionic-native/google-maps'
-import { animation } from '@angular/core/src/animation/dsl';
+import {Component} from '@angular/core';
+import {NavController , Platform} from 'ionic-angular';
+import { 
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  Marker,
+  LatLng
+} from '@ionic-native/google-maps'; 
 
 
-/**
- * Generated class for the MapaCinemasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-mapa-cinemas',
   templateUrl: 'mapa-cinemas.html',
 })
 export class MapaCinemasPage {
-
-  public latitude: number;
-  public longitude: number;
-
-
-
   map: GoogleMap;
 
-  constructor(public navCtrl: NavController,
-     public navParams: NavParams,
-    private geolocation: Geolocation,
-    private sanitizer: DomSanitizer,
-    private platform: Platform
-  ) {
-    this.platform.ready().then(()=>{
-      this.minhaLocalizacao();
-      this.loadMap();
-    })
-  }
-  
-  
 
-  ionViewDidLoad() {
-      this.minhaLocalizacao();
-      this.loadMap();
-  }
-
-  loadMap()
+  constructor( public navCtrl: NavController, private platform: Platform)
   {
 
-    let opcoes : GoogleMapOptions = {
+  }
 
-      camera: {
-        target: {
-          lat: this.latitude,
-          lng:this.longitude
+  ionViewDidLoad(){
+   this.platform.ready().then( () => {
+     this.loadMap();
+   });
+  }
+
+  loadMap(){
+
+    let mapElement = document.getElementById('map');
+    let mapOptions: GoogleMapOptions = {
+      'camera': {
+        'target': {
+          "lat": 21.382314,
+          "lng": -157.933097
         },
-        zoom: 18,
-        tilt: 30
+        'zoom': 10
       }
     };
 
-    this.map = GoogleMaps.create('map_canvas',opcoes);
-
-    let desenho: Marker = this.map.addMarkerSync({
-      title: 'Ionic',
-      icon: 'blue',
-      animation: 'DROP',
-      position: {
-        lat: this.latitude,
-        lng: this.longitude
-      }
-    });
-    
+    this.map = GoogleMaps.create(mapElement, mapOptions);
 
     this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
-      console.log('Mapa Pronto');
-    })
-    console.log('Entrei no Mapa');
-  }
+      this.map.on(GoogleMapsEvent.MAP_CLICK).subscribe( () => {
+        console.log('Map Clicked');
+      });
 
-  minhaLocalizacao()
-  {
-    this.geolocation.getCurrentPosition().then((resp) => {
+      this.map.on(GoogleMapsEvent.MAP_DRAG_START).subscribe( () => {
+        console.log('map drag start');
+      });
 
-      this.latitude = resp.coords.latitude;
-      this.longitude = resp.coords.longitude
-    }).catch((error) => {
-      console.log("Erro Location",error)
+      this.map.on(GoogleMapsEvent.MAP_DRAG_END).subscribe( () => {
+        console.log('map drag end');
+      });
+
+      return this.map.addMarker({
+        title: 'Locations S',
+        icon: 'blue',
+        animation: 'DROP',
+        position: new LatLng(21.382314, -157.933097)
+      })
+    }).then((marker:Marker) => {
+      console.log('marker added');
+      marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+        alert('clicked');
+      });
+
+      marker.on(GoogleMapsEvent.MARKER_DRAG_START).subscribe( () => {
+        console.log('marker drag start');
+      });
+
+      marker.on(GoogleMapsEvent.MARKER_DRAG_END).subscribe( () => {
+        console.log('marker drag end');
+      })
     });
+
   }
-
-
-
+  
 
 }
