dd8c11cef460477054ec5693e4e1260ad54388ab
Manoel <manoeltavares.jr@gmail.com>
2018-07-24 23:51:51 -0300

    Apk 2407
    Geração de Apk gera Alterações desconsideráveis.

--- PrimeiroApp/src/providers/serie/serie.ts
+++ PrimeiroApp/src/providers/serie/serie.ts
@@ -2,12 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-/*
-  Generated class for the SerieProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class SerieProvider {
 
